1367b757ee4a2bb29e071537e0a8d3855b5433cc
Miguel Martelo <miguemartelo1031@gmail.com>
2023-05-06 18:47:06 -0500

    :sparkles: show the pages component in the App component

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,9 +1,20 @@
+import Home from '../Home';
+import MyAccount from '../MyAccount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import NotFound from '../NotFound';
+import SignIn from '../SignIn';
 import './App.css';
 
 function App() {
   return (
     <div className='bg-red-300'>
-      <h1>Hello E-commerce</h1>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
     </div>
   );
 }
