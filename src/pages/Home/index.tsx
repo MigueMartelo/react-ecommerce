f8a1e4557bfa12a91808925a1c181dadf35c97d2
Miguel Martelo <miguemartelo1031@gmail.com>
2023-05-08 20:47:38 -0500

    :sparkles: add Card component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,12 @@
 import { Layout } from '../../components/Layout';
+import { Card } from '../../components/Card/index';
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      Home
+      <Card />
+    </Layout>
+  );
 };
 export default Home;
