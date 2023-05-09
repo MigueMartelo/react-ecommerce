5080937492e0ffe2cd3c16054693671fefa51797
Miguel Martelo <miguemartelo1031@gmail.com>
2023-05-08 21:30:25 -0500

    :art: add bg to the Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 export const Navbar = () => {
   const activeStyle = 'underline underline-offset-4';
   return (
-    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm top-0'>
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm top-0 bg-white'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold'>
           <NavLink
