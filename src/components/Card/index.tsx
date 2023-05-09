d7268809d47c7e3ff61736b3f44ded99e157b5fc
Miguel Martelo <miguemartelo1031@gmail.com>
2023-05-08 21:11:44 -0500

    :sparkles: fetch and show the products from the API

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,19 @@
-export const Card = () => {
+import { Product } from '../../interfaces/index';
+
+interface CardProps {
+  product: Product;
+}
+
+export const Card = ({ product }: CardProps) => {
   return (
     <div className='bg-white cursor-pointer w-56 h-60'>
       <figure className='relative mb-4 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-2 py-0,5'>
-          Electronics
+          {product.category.name}
         </span>
         <img
-          src='https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-          alt='Headphones'
+          src={product.images[0]}
+          alt={product.description}
           className='w-full h-full object-cover rounded-lg'
         />
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
@@ -15,8 +21,8 @@ export const Card = () => {
         </div>
       </figure>
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+        <span className='text-sm font-light'>{product.title}</span>
+        <span className='text-lg font-medium'>${product.price}</span>
       </p>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import { Layout } from '../../components/Layout';
 import { Card } from '../../components/Card/index';
+import { Product } from '../../interfaces';
 
 const Home = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      {products &&
+        products.map((product) => <Card key={product.id} product={product} />)}
     </Layout>
   );
 };
